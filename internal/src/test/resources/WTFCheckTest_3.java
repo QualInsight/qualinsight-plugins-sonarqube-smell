4b48e025b29069498231498e4c38225c76e46f49
michel <michel.pawlak@gmail.com>
2015-09-15 21:34:18 +0200

    #15 bugfix

--- internal/src/test/resources/WTFCheckTest_3.java
+++ internal/src/test/resources/WTFCheckTest_3.java
@@ -3,25 +3,70 @@ package test;
 import com.qualinsight.plugins.sonarqube.wtf.api.annotation.WTF;
 import com.qualinsight.plugins.sonarqube.wtf.api.model.WTFType;
 
-@WTF(minutes=10,reason="Anti-pattern",type=WTFType.ANTI_PATTERN) // Noncompliant {{Anti-pattern}}
-public class WTFCheckTest_1 {
-    
-    @WTF(minutes=10,reason="Anti-pattern",type=WTFType.ANTI_PATTERN) // Noncompliant {{Anti-pattern}}
+// Noncompliant @+1 {{Anti-pattern}}
+@WTF(minutes = 10, reason = "Anti-pattern", type = WTFType.ANTI_PATTERN)
+public class WTFCheckTest_3 {
+
+    public static final String CONSTANT1 = "Anti-";
+
+    public static final String CONSTANT2 = "pattern";
+
+    public static final String CONSTANT3 = "Anti-pattern";
+
+    // Noncompliant @+1 {{Anti-pattern}}
+    @WTF(minutes = 10, reason = "Anti-pattern", type = WTFType.ANTI_PATTERN)
     Object field1;
-    
-    @WTF(minutes=10,reason="Anti-pattern",type=WTFType.ANTI_PATTERN) // Noncompliant {{Anti-pattern}}
-    public void method() {}
-    
-    @WTF(minutes=10,reason="Anti-pattern",type=WTFType.ANTI_PATTERN) // Noncompliant {{Anti-pattern}}
+
+    // Noncompliant @+1 {{Anti-pattern}}
+    @WTF(minutes = 10, reason = "Anti-pattern", type = WTFType.ANTI_PATTERN)
+    public void method() {
+    }
+
+    // Noncompliant @+1 {{Anti-pattern}}
+    @WTF(minutes = 10, reason = "Anti-" + "pattern", type = WTFType.ANTI_PATTERN)
+    public void method2() {
+    }
+
+    // Noncompliant @+1 {{Anti-pattern}}
+    @WTF(minutes = 10, reason = "Anti" + "-" + "pattern", type = WTFType.ANTI_PATTERN)
+    public void method2() {
+    }
+
+    // Noncompliant @+1 {{Anti-pattern}}
+    @WTF(minutes = 10, reason = CONSTANT1 + "pattern", type = WTFType.ANTI_PATTERN)
+    public void method4() {
+    }
+
+    // Noncompliant @+1 {{Anti-pattern}}
+    @WTF(minutes = 10, reason = "Anti-" + CONSTANT2, type = WTFType.ANTI_PATTERN)
+    public void method3() {
+    }
+
+    // Noncompliant @+1 {{Anti-pattern}}
+    @WTF(minutes = 10, reason = CONSTANT1 + CONSTANT2, type = WTFType.ANTI_PATTERN)
+    public void method5() {
+    }
+
+    // Noncompliant @+1 {{Anti-pattern}}
+    @WTF(minutes = 10, reason = CONSTANT3, type = WTFType.ANTI_PATTERN)
+    public void method6() {
+    }
+
+    // Noncompliant @+1 {{Anti-pattern}}
+    @WTF(minutes = 10, reason = "Anti-pattern", type = WTFType.ANTI_PATTERN)
     enum EnumType {
         A,
-        @WTF(minutes=10,reason="Anti-pattern",type=WTFType.ANTI_PATTERN) // Noncompliant {{Anti-pattern}}
+        // Noncompliant @+1 {{Anti-pattern}}
+        @WTF(minutes = 10, reason = "Anti-pattern", type = WTFType.ANTI_PATTERN)
         B
     }
-    
-    @WTF(minutes=10,reason="Anti-pattern",type=WTFType.ANTI_PATTERN) // Noncompliant {{Anti-pattern}}
+
+    // Noncompliant @+1 {{Anti-pattern}}
+    @WTF(minutes = 10, reason = "Anti-pattern", type = WTFType.ANTI_PATTERN)
     public class AnnotatedDummy {
-        @WTF(minutes=10,reason="Anti-pattern",type=WTFType.ANTI_PATTERN) // Noncompliant {{Anti-pattern}}
+
+        // Noncompliant @+1 {{Anti-pattern}}
+        @WTF(minutes = 10, reason = "Anti-pattern", type = WTFType.ANTI_PATTERN)
         String s;
     }
 }
